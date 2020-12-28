c8ce338830197466d2618b1cdde8296e3448eca6
Alexander Cyon <alex.cyon@gmail.com>
2020-12-28 16:44:17 +0100

    Add jest as plugin to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,16 +10,18 @@ module.exports = {
   },
   plugins: [
     '@typescript-eslint',
-    "functional",
+    'jest',
+    'functional',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    "plugin:functional/external-recommended", // https://github.com/jonaskello/eslint-plugin-functional
-    "plugin:functional/recommended", // https://github.com/jonaskello/eslint-plugin-functional
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:jest/recommended',
+    'plugin:functional/external-recommended', // https://github.com/jonaskello/eslint-plugin-functional
+    'plugin:functional/recommended', // https://github.com/jonaskello/eslint-plugin-functional
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   env: {
     node: true
--- packages/radix-subatomic/src/index.ts
+++ packages/radix-subatomic/src/index.ts
@@ -1,5 +1,1 @@
-module.exports = radixSubatomic
-
-function radixSubatomic() {
-	// TODO
-}
+// TODO
