ce1a4eff073a387db34bf8eea2ddcd0d66c59dc1
Alexander Cyon <alex.cyon@gmail.com>
2020-12-28 16:46:41 +0100

    formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,64 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: [
-        './packages/*/tsconfig.json',
-        './packages/*/tsconfig.test.json'
-    ],
-  },
-  plugins: [
-    '@typescript-eslint',
-    'jest',
-    'functional',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:jest/recommended',
-    'plugin:functional/external-recommended', // https://github.com/jonaskello/eslint-plugin-functional
-    'plugin:functional/recommended', // https://github.com/jonaskello/eslint-plugin-functional
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  env: {
-    node: true
-  },
-  rules: {
-    "complexity": ["error", 2],
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: [
+			'./packages/*/tsconfig.json',
+			'./packages/*/tsconfig.test.json',
+		],
+	},
+	plugins: ['@typescript-eslint', 'jest', 'functional'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:jest/recommended',
+		'plugin:functional/external-recommended', // https://github.com/jonaskello/eslint-plugin-functional
+		'plugin:functional/recommended', // https://github.com/jonaskello/eslint-plugin-functional
+		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+	],
+	env: {
+		node: true,
+	},
+	rules: {
+		complexity: ['error', 2],
 
-    "max-depth": ["error", 2],
+		'max-depth': ['error', 2],
 
-    "max-lines": [
-      "error", {
-        "max": 100, 
-        "skipBlankLines": true,
-        "skipComments": true,
-      }
-    ],
-    
-    "max-lines-per-function": [
-      "error", {
-        "max": 20,
-        "skipBlankLines": true,
-        "skipComments": true,
-        "IIFEs": true
-      }
-    ],
+		'max-lines': [
+			'error',
+			{
+				max: 100,
+				skipBlankLines: true,
+				skipComments: true,
+			},
+		],
 
-    "max-nested-callbacks": ["error", 3],
+		'max-lines-per-function': [
+			'error',
+			{
+				max: 20,
+				skipBlankLines: true,
+				skipComments: true,
+				IIFEs: true,
+			},
+		],
 
-    "max-params": ["error", 1],
+		'max-nested-callbacks': ['error', 3],
 
-    // ESLint-Plugin-Functional RULES
-    "functional/immutable-data": "error",
-    "functional/no-let": "error",
-    "functional/no-loop-statement": "error",
-    "no-param-reassign": "error",
-    "functional/no-try-statement": "error",
+		'max-params': ['error', 1],
 
-    // Disabled because '*.test.ts' files fail to lint 'describe' functions second argument (anonymous function).
-    "functional/no-expression-statement": "off",
-    "functional/functional-parameters": "off",
-  }
-};
+		// ESLint-Plugin-Functional RULES
+		'functional/immutable-data': 'error',
+		'functional/no-let': 'error',
+		'functional/no-loop-statement': 'error',
+		'no-param-reassign': 'error',
+		'functional/no-try-statement': 'error',
+
+		// Disabled because '*.test.ts' files fail to lint 'describe' functions second argument (anonymous function).
+		'functional/no-expression-statement': 'off',
+		'functional/functional-parameters': 'off',
+	},
+}
--- jest.build.config.js
+++ jest.build.config.js
@@ -1,27 +1,27 @@
 const config = {
-  browser: true,
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testMatch: ['<rootDir>/packages/**/test/?(*.)+(spec|test).ts'],
-  moduleDirectories: [
-    '<rootDir>/includes',
-    '<rootDir>/node_modules',
-    '<rootDir>/*/node_modules',
-  ],
-  moduleFileExtensions: ['js', 'ts', 'node', 'json'],
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-      tsconfig: './tsconfig.test.json',
-    },
-  },
-  testURL: 'http://localhost',
-  setupFiles: ['<rootDir>/jest-setup.js'],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-  ],
-};
+	browser: true,
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	testMatch: ['<rootDir>/packages/**/test/?(*.)+(spec|test).ts'],
+	moduleDirectories: [
+		'<rootDir>/includes',
+		'<rootDir>/node_modules',
+		'<rootDir>/*/node_modules',
+	],
+	moduleFileExtensions: ['js', 'ts', 'node', 'json'],
+	globals: {
+		'ts-jest': {
+			babelConfig: true,
+			tsconfig: './tsconfig.test.json',
+		},
+	},
+	testURL: 'http://localhost',
+	setupFiles: ['<rootDir>/jest-setup.js'],
+	watchPlugins: [
+		'jest-watch-typeahead/filename',
+		'jest-watch-typeahead/testname',
+	],
+}
 
-module.exports = config;
+module.exports = config
