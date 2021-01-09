fc895fa1cfa8314d75e1dee6751a53efe8ad596c
Alexander Wormbs <alexander.wormbs@gmail.com>
2021-01-09 14:28:23 +0000

    formatting

--- packages/primitives/src/_index.ts
+++ packages/primitives/src/_index.ts
@@ -1,4 +1,4 @@
-export { uint256FromBN, fitsInUInt256 } from './uint256'
+export { uint256FromBN, fitsInUInt256 } from './UInt256'
 export { resultToAsync } from './resultAsync'
 export * from './byte'
 export * from './_types'
