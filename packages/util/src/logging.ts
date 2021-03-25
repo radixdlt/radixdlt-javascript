f1f19beb0b533e0f8438f9e2d78da74ab345988e
Alexander Wormbs <alexander.wormbs@gmail.com>
2021-03-25 13:42:33 +0000

    format

--- packages/util/src/logging.ts
+++ packages/util/src/logging.ts
@@ -3,39 +3,44 @@ import chalk from 'chalk'
 import prefix from 'loglevel-plugin-prefix'
 
 export enum LogLevel {
-    DEBUG = 'debug',
-    ERROR = 'error',
-    WARN = 'warn',
-    INFO = 'info',
-    SILENT = 'silent',  
+	DEBUG = 'debug',
+	ERROR = 'error',
+	WARN = 'warn',
+	INFO = 'info',
+	SILENT = 'silent',
 }
 
 log.setLevel('warn')
 
 const meta = {
-    info: {
-        color: chalk.blue,
-        emoji: '💙'
-    },
-    warn: {
-        color: chalk.yellow,
-        emoji: '💛'
-    },
-    error: {
-        color: chalk.red,
-        emoji: '❤'
-    },
-    debug: {
-        color: chalk.italic.cyan,
-        emoji: '💻'
-    }
+	info: {
+		color: chalk.blue,
+		emoji: '💙',
+	},
+	warn: {
+		color: chalk.yellow,
+		emoji: '💛',
+	},
+	error: {
+		color: chalk.red,
+		emoji: '❤',
+	},
+	debug: {
+		color: chalk.italic.cyan,
+		emoji: '💻',
+	},
 }
 
 prefix.reg(log)
 
 prefix.apply(log, {
-    format: (level, name, timestamp) => `${chalk.gray(`[${timestamp}]`)} ${meta[level.toLowerCase() as Exclude<LogLevel, 'silent' | 'all'>].emoji} ${meta[level.toLowerCase() as Exclude<LogLevel, 'silent' | 'all'>].color(level)}`
+	format: (level, name, timestamp) =>
+		`${chalk.gray(`[${timestamp}]`)} ${
+			meta[level.toLowerCase() as Exclude<LogLevel, 'silent'>]
+				.emoji
+		} ${meta[
+			level.toLowerCase() as Exclude<LogLevel, 'silent'>
+		].color(level)}`,
 })
 
 export { log }
-
