36dfbdfe73f3c8c5b7e9f26e46cf0e2f3ecf5112
Alexander Wormbs <alexander.wormbs@gmail.com>
2021-04-01 12:02:58 +0100

    remove convoluted hex thingy

--- packages/application/test/mockRadix.ts
+++ packages/application/test/mockRadix.ts
@@ -396,40 +396,13 @@ const randomPendingTransaction = (
 	const prng = detPRNGWithBuffer(Buffer.from(signedTx.transaction.blob))
 	const shouldFail = prng() % 2 > 0
 
-	const rand32ByteHex = (() => {
-		let hex = ''
-		for (let i = 0; i < 64; i++) {
-			hex = hex.concat(
-				[
-					'0',
-					'1',
-					'2',
-					'3',
-					'4',
-					'5',
-					'6',
-					'7',
-					'8',
-					'9',
-					'a',
-					'b',
-					'c',
-					'd',
-					'e',
-					'f',
-				][prng() % 16],
-			)
-		}
-		return hex
-	})()
-
 	return shouldFail
 		? {
 				failure: 'Failed',
 		  }
 		: {
 				txID: TransactionIdentifier.create(
-					rand32ByteHex,
+					sha256(Buffer.from(signedTx.transaction.blob)),
 				)._unsafeUnwrap(),
 		  }
 }
