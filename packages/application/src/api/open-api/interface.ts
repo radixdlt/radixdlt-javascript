c76dc77faa82dd5a0851f8c27b6ff1143bf22e46
AbstractFruitFactory <alexander.wormbs@gmail.com>
2021-11-26 01:07:22 +0000

    finalize interface

--- packages/application/src/api/open-api/interface.ts
+++ packages/application/src/api/open-api/interface.ts
@@ -4,32 +4,37 @@ import {
 	OpenApiClientCall,
 	ReturnOfAPICall,
 } from '@radixdlt/networking'
-import { handleNativeTokenResponse, handleNetworkResponse } from './responseHandlers'
+import { handleAccountBalancesResponse, handleAccountTransactionsResponse, handleBuildTransactionResponse, handleDeriveTokenIdentifierResponse, handleNativeTokenResponse, handleNetworkResponse, handleStakePositionsResponse, handleSubmitTransactionResponse, handleTokenInfoResponse, handleTransactionRulesResponse, handleUnstakePositionsResponse, handleValidatorResponse, handleValidatorsResponse } from './responseHandlers'
 import { pipe } from 'ramda'
-import { Result } from 'neverthrow'
-import { handleTokenInfoResponse } from '..'
+import { Result, ResultAsync } from 'neverthrow'
 
 const callAPIWith =
 	(call: OpenApiClientCall) =>
-	<DecodedResponse, M extends MethodName>(method: M) =>
-	(handleResponse: (response: ReturnOfAPICall<M>) => Result<DecodedResponse, Error[]>) =>
-		(params: InputOfAPICall<M>) =>
+	<M extends MethodName>(method: M) =>
+	<DecodedResponse>(handleResponse: (response: ReturnOfAPICall<M>) => Result<DecodedResponse, Error[]>) =>
+		(params: InputOfAPICall<M>): ResultAsync<DecodedResponse, Error[]> =>
 			pipe(
 				() => call(method, params),
-				result => result.map(handleResponse),
+				result => result.mapErr(e => [e]).andThen(handleResponse),
 			)()
 
 export const getAPI = pipe(
 	(call: OpenApiClientCall) => callAPIWith(call),
 	callAPI => ({
-		getNetwork: callAPI('networkPost')(handleNetworkResponse),
-		getTokenInfo: callAPI('tokenPost')(handleTokenInfoResponse),
-		getNativeTokenInfo: callAPI('tokenNativePost')(handleNativeTokenResponse)
+		network: callAPI('networkPost')(handleNetworkResponse),
+		tokenInfo: callAPI('tokenPost')(handleTokenInfoResponse),
+		nativeTokenInfo: callAPI('tokenNativePost')(handleNativeTokenResponse),
+		deriveTokenIdentifier: callAPI('tokenDerivePost')(handleDeriveTokenIdentifierResponse),
+		accountBalances: callAPI('accountBalancesPost')(handleAccountBalancesResponse),
+		stakePositions: callAPI('accountStakesPost')(handleStakePositionsResponse),
+		unstakePositions: callAPI('accountUnstakesPost')(handleUnstakePositionsResponse),
+		accountTransactions: callAPI('accountTransactionsPost')(handleAccountTransactionsResponse),
+		validator: callAPI('validatorPost')(handleValidatorResponse),
+		validators: callAPI('validatorsPost')(handleValidatorsResponse),
+		transactionRules: callAPI('transactionRulesPost')(handleTransactionRulesResponse),
+		buildTransaction: callAPI('transactionBuildPost')(handleBuildTransactionResponse),
+		// finalizeTransaction: callAPI('transactionFinalizePost')(handleFinalizeTransactionResponse),
+		submitTransaction: callAPI('transactionSubmitPost')(handleSubmitTransactionResponse),
+		// transactionStatus: callAPI('transactionStatusPost')(handleTransactionStatusResponse)
 	})
 )
-
-// getAPI(openApiClient(new URL('')).call)
-// 	.networkPost({ body: {} })
-// 	.then(res => {})
-
-// openApiClient(new URL('')).call('accountBalancesPost', { a })
